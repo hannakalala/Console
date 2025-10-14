52f6a3b430c8165bcedbb83db6b2985d1986e0d0
hannakalala <ylivalo@gmail.com>
2025-10-14 17:23:41 +0300

    init css

---
+++ src/js/terminal.js
@@ -0,0 +1,14 @@
+
+var typed = new Typed('#typed', {
+    strings: ['Hailing...','Welcome to the Space Shuttle Orientation!','Please fasten your seatbelt, and breathe normally.','Enjoy your flight!'],
+    typeSpeed: 35,
+    startDelay: 500,
+    backDelay: 1000,
+    backSpeed: 35,
+    fadeOut: true,
+    loop: true,
+    loopCount: Infinity,
+    // showCursor: false
+    // cursorChar: '...'
+
+});
